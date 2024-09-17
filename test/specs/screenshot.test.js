c78e47ec04acb0f02ba35e5b09791b649dfda5cc
rattus-aristarchus <lankin.m.a@gmail.com>
2024-09-17 11:43:21 +0300

    Update screenshot.test.js

--- test/specs/screenshot.test.js
+++ test/specs/screenshot.test.js
@@ -1,11 +1,18 @@
 const { remote } = require('webdriverio')
 
 it('Sample test case', async function(){
-    const browser = await remote()
+    const browser = await remote({
+        capabilities: {
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                args: process.env.CI ? ['headless', 'disable-gpu'] : []
+            }
+        }
+    })
 
     await browser.url('https://en.wikipedia.org/wiki/Software_testing')
 
     await browser.saveScreenshot('./screenshot.png')
     await browser.deleteSession()
-
+    
 });
